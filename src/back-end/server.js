4a147c4212467e9a2806d706346a2ae0d32dea08
hyoneekim <97020327+hyoneekim@users.noreply.github.com>
2025-01-03 22:40:19 +0200

    trying globally_v2

--- src/back-end/server.js
+++ src/back-end/server.js
@@ -70,15 +70,14 @@ app.post('/api/guests', async (req, res) => {
 });
 
 // Serve React App in Production
-if (process.env.NODE_ENV === 'production') {
   // Serve the static files from the React app
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
-}
+
 
 // Start the Server
 app.listen(PORT, () => {
